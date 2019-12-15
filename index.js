093081f010d41d9fb2b99fe15a64629e057f39f7
White2993 <47195867+White2993@users.noreply.github.com>
2019-12-15 15:06:07 -0600

    Added all prompts

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const connection = mysql.createConnection({
   database: 'db_employeetracker',
   password: 'Goldensphinx596!'
 });
+var role_list = [];
 
 connection.connect(function(err) {
   if (err) throw err;
@@ -23,8 +24,6 @@ function startApp(cb) {
   }
 }
 
-
-
 function questions() {
 inquirer
   .prompt([
@@ -33,12 +32,12 @@ inquirer
       message: 'WHAT WOULD YOU LIKE TO DO?',
       name: 'init_question',
       choices: [
-        'ADD DEPARTMENT', 
+        'ADD DEPARTMENT',
+        'VIEW ALL DEPARTMENTS', 
         'ADD ROLE', 
-        'ADD EMPLOYEE', 
-        'VIEW DEPARTMENT', 
-        'VIEW ROLE', 
-        'VIEW EMPLOYEE', 
+        'VIEW ALL ROLES',
+        'ADD EMPLOYEE',
+        'VIEW ALL EMPLOYEES', 
         'UPDATE EMPLOYEE ROLE'
       ]
     }
@@ -47,19 +46,12 @@ inquirer
       inquirer
         .prompt([
           {
-            message: 'WOULD YOU LIKE TO ADD OR VIEW DEPARTMENTS?',
-            name: 'addorview'
+            message: 'WHAT DEPARTMENT WOULD YOU LIKE TO ADD?',
+            name: 'add_dept'
           }
         ]).then(x => {
-          if(x.addorview === 'ADD'){
-            inquirer.prompt([
-              {
-                message: 'WHAT DEPARTMENT WOULD YOU LIKE TO ADD?',
-                name: 'add_dept'
-              }
-            ]).then(x => {
-              if(x.add_dept)
-              connection.query("INSERT INTO department(name) VALUES (?)", [x.add_dept]);
+          if(x.add_dept){
+            connection.query("INSERT INTO department(name) VALUES (?)", [x.add_dept]);
               inquirer.prompt([
                 {
                   type: 'confirm',
@@ -70,41 +62,114 @@ inquirer
                 notDone = answer.restart;
                 startApp(questions);
               });
-            });
-          };
+            };
           });
 
 
-    } else if (answer.init_question === 'ADD ROLE') {
-      inquirer.prompt([
-        {
-          message: 'WHAT ROLE WOULD YOU LIKE TO ADD?',
-          name: 'addrole'
-        }
-      ])
-      .then(answer => {
-        connection.query("INSERT INTO employee(role_id) VALUES (?)", [answer.addrole]);
+    } else if (answer.init_question === 'VIEW ALL DEPARTMENTS') {
+      connection.query(`SELECT * FROM department`, function(err, result) {
+        if (err) throw err
+        console.log(result)
         inquirer.prompt([
           {
             type: 'confirm',
             message: 'WOULD YOU LIKE TO DO ANYTHING ELSE?',
             name: 'restart'
           }
-        ])
-        .then(answer => {
+        ]).then(answer => {
           notDone = answer.restart;
           startApp(questions);
         });
+      });    
+    
+    
+    } else if (answer.init_question === 'ADD ROLE') {
+      inquirer.prompt([
+        {
+          message: 'WHAT ROLE WOULD YOU LIKE TO ADD?',
+          name: 'role'
+        },
+        {
+          message: 'WHAT IS THE SALARY OF THIS POSITION?',
+          name: 'salary'
+        },
+        {
+          message: 'WHAT IS THE DEPARTMENT ID OF THIS ROLE?',
+          name: 'department_id'
+        }
+      ]).then(answer => {
+        connection.query(`INSERT INTO employee_role(title, salary, department_id) VALUES (???)`, [answer.role], [answer.salary], [answer.department_id])
+        role_list.push(Object.assign({}, answer));
+        console.log(role_list);
+        if(answer) {
+        inquirer.prompt([
+          {
+            type: 'confirm',
+            message: 'WOULD YOU LIKE TO DO ANYTHING ELSE?',
+            name: 'restart'
+          }
+        ]).then(answer => {
+          notDone = answer.restart;
+          startApp(questions);
+        });
+      }
       })
-    }
-    });
+    
+    
+    } else if (answer.init_question === 'VIEW ALL ROLES') {
+      connection.query(`SELECT * FROM employee_role`, function(err, result) {
+        if (err) throw err
+        console.log(result)
+        inquirer.prompt([
+          {
+            type: 'confirm',
+            message: 'WOULD YOU LIKE TO DO ANYTHING ELSE?',
+            name: 'restart'
+          }
+        ]).then(answer => {
+          notDone = answer.restart;
+          startApp(questions);
+        });
+      }); 
+    
+    
+    } else if (answer.init_question === 'ADD EMPLOYEE') {
+      inquirer.prompt([
+        {
+          message: 'PLEASE ENTER FIRST NAME',
+          name: 'first_name'
+        }, 
+        {
+          message: 'PLEASE ENTER LAST NAME',
+          name: 'last_name'
+        },
+        {
+          message: 'PLEASE ENTER ROLE ID',
+          name: 'role_id'
+        },
+        {
+          message: 'PLEASE ENTER MANAGER ID',
+          name: 'manager_id'
+        }
+      ]).then(answer => {
+          connection.query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?)`, [answer.first_name], [answer.last_name], [answer.role_id], [answer.manager_id])
+          console.log(answer)
+          inquirer.prompt([
+            {
+              type: 'confirm',
+              message: 'WOULD YOU LIKE TO DO ANYTHING ELSE?',
+              name: 'restart'
+            }
+          ]).then(answer => {
+            notDone = answer.restart;
+            startApp(questions);
+          });
+        })
+      } 
+    })
   
   
   
   
-  
-  
-  
-  };
-
+};
 startApp(questions);
